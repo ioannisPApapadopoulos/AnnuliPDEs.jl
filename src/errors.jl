f881e3abc22fb0b36c8f1c0f6e89f4064f71f2af
ioannisPApapadopoulos <john.papadopoulos0@gmail.com>
2023-08-08 01:00:15 +0100

    SEM examples

--- src/errors.jl
+++ src/errors.jl
@@ -189,63 +189,61 @@ function _collect_errors(U::Adjoint{T, Matrix{T}}, Uₐ::Adjoint{T, Matrix{T}}, 
 end
 
 # Works for both Chebyshev and Two-band
-function collect_errors(TFρ::Tuple, X::AbstractMatrix, ua::Function, errors=[], a=1)
+function collect_errors(TFρ::Tuple{<:Any, <:Fourier, <:Number}, X::AbstractMatrix, ua::Function, errors=[], a=1)
 
     # One cell routine
-    if length(TFρ) == 3
-        (T, F, ρ) = TFρ
-        try
-            @assert T.parent isa ChebyshevT
-        catch 
-            @assert T.P isa TwoBandJacobi
-        end
-        @assert F isa Fourier
-        @assert ρ isa Number
-
-        V = promote_type(eltype(T), eltype(F))
-
-        n = size(X,1)-2
-        Z = ZernikeAnnulus{V}(ρ,a,a) # want to measure the errors on the ZernikeAnnulus grid
-
-        g = AnnuliOrthogonalPolynomials.grid(Z, Block(n))
-        p = g -> [g.r, g.θ]; rθ = map(p, g); rs = first.(rθ)[:,1]; θs = last.(rθ)[1,:]
-        # Compute values of the solution on the grid
-        Uu = (F[θs,1:size(X,2)]*(T[rs,1:size(X,1)]*X)')' # Directly expand the tensor-product basis on the grid
-        return _collect_errors(Uu, θs, rs, ua, errors)
-    # Two cell routine
-    elseif length(TFρ) == 4
-        (T, Tₐ, F, ρ) = TFρ
-        @assert T.parent isa ChebyshevT && Tₐ.parent isa ChebyshevT
-        @assert F isa Fourier
-        @assert ρ isa Number
-
-        V = promote_type(eltype(T), eltype(Tₐ), eltype(F))
-
-        n = (size(X,1)-2)÷2
-        # When doing spectral element, we use these grids instead
-        Z = Zernike{V}(a-1,a-1)
-        Zₐ = ZernikeAnnulus{V}(ρ,a-1,a-1)
-        
-        p = g -> [g.r, g.θ];
-        g = AnnuliOrthogonalPolynomials.grid(Z, Block(n))
-        gₐ = AnnuliOrthogonalPolynomials.grid(Zₐ, Block(n))
-        rθ = map(p, g); rs = ρ.*first.(rθ)[:,1]; θs = last.(rθ)[1,:]
-        rθₐ = map(p, gₐ); rsₐ = first.(rθₐ)[:,1]; θsₐ = last.(rθₐ)[1,:]
-    
-        Uu = (F[θs,1:2n]*(T[rs,1:n+1]*X[n+2:end,1:2n])')'
-        Uuₐ = (F[θsₐ,1:2n]*(Tₐ[rsₐ,1:n+1]*X[1:n+1,1:2n])')'
-        
-        return _collect_errors(Uu, Uuₐ, θs, rs, rsₐ, ua, errors)
-    else
-        error("Collect error not implemented for these arguments.")
+    (T, F, ρ) = TFρ
+    try
+        @assert T.parent isa ChebyshevT
+    catch
+        @assert T.P isa TwoBandJacobi
     end
+    @assert F isa Fourier
+    @assert ρ isa Number
 
+    V = promote_type(eltype(T), eltype(F))
+
+    n = size(X,1)-2
+    Z = ZernikeAnnulus{V}(ρ,a,a) # want to measure the errors on the ZernikeAnnulus grid
+
+    g = AnnuliOrthogonalPolynomials.grid(Z, Block(n))
+    p = g -> [g.r, g.θ]; rθ = map(p, g); rs = first.(rθ)[:,1]; θs = last.(rθ)[1,:]
+    # Compute values of the solution on the grid
+    Uu = (F[θs,1:size(X,2)]*(T[rs,1:size(X,1)]*X)')' # Directly expand the tensor-product basis on the grid
+    return _collect_errors(Uu, θs, rs, ua, errors)
+end
+
+function collect_errors(TFρ::Tuple{<:Any, <:Any, <:Fourier, <:Number}, X::AbstractMatrix, ua::Function, errors=[], a=1)
+
+    # Two cell routine
+    (T, Tₐ, F, ρ) = TFρ
+    @assert T.parent isa ChebyshevT && Tₐ.parent isa ChebyshevT
+    @assert F isa Fourier
+    @assert ρ isa Number
+
+    V = promote_type(eltype(T), eltype(Tₐ), eltype(F))
+
+    n = (size(X,1)-2)÷2
+    # When doing spectral element, we use these grids instead
+    Z = Zernike{V}(a-1,a-1)
+    Zₐ = ZernikeAnnulus{V}(ρ,a-1,a-1)
+    
+    p = g -> [g.r, g.θ];
+    g = AnnuliOrthogonalPolynomials.grid(Z, Block(n))
+    gₐ = AnnuliOrthogonalPolynomials.grid(Zₐ, Block(n))
+    rθ = map(p, g); rs = ρ.*first.(rθ)[:,1]; θs = last.(rθ)[1,:]
+    rθₐ = map(p, gₐ); rsₐ = first.(rθₐ)[:,1]; θsₐ = last.(rθₐ)[1,:]
+
+    Uu = (F[θs,1:2n]*(T[rs,1:n+1]*X[n+2:end,1:2n])')'
+    Uuₐ = (F[θsₐ,1:2n]*(Tₐ[rsₐ,1:n+1]*X[1:n+1,1:2n])')'
+
+    return _collect_errors(Uu, Uuₐ, θs, rs, rsₐ, ua, errors)
 end
 
 ###
 # Collect error routines for the Chebyshev-Fourier series + Zernike
 ###
-function collect_errors(TFZρ::Tuple{<:Any, <:Fourier, <:TV}, Xu::Tuple{Matrix{TV}, ModalTrav{TV, Matrix{TV}}}, ua::Function, errors=[]) where TV
+function collect_errors(TFZρ::Tuple{<:Any, <:Fourier, <:Zernike, <:Number}, Xu::Tuple{Matrix{TV}, ModalTrav{TV, Matrix{TV}}}, ua::Function, errors=[]) where TV
 
     (T, F, Z, ρ) = TFZρ
     (X, u) = Xu
@@ -261,7 +259,7 @@ end
 ###
 
 # helper
-function collect_errors(TFαρ::Tuple{<:Any, <:Fourier, <:TV, <:TV}, X::AbstractMatrix, ua::Function, errors=[]) where TV
+function collect_errors(TFαρ::Tuple{<:Any, <:Fourier, <:Number, <:Number}, X::AbstractMatrix, ua::Function, errors=[])
 
     (T, F, α, ρ) = TFαρ
     @assert T.parent isa ChebyshevT 
@@ -281,14 +279,14 @@ function collect_errors(TFαρ::Tuple{<:Any, <:Fourier, <:TV, <:TV}, X::Abstract
 
 end
 
-function collect_errors(ZTFαρ::Tuple, uX::Tuple{ModalTrav{TV, Matrix{TV}}, Matrix{TV}}, ua::Function, errors=[]) where TV
+function collect_errors(ZTFαρ::Tuple{<:ZernikeAnnulus, <:Any, <:Fourier, <:Number, <:Number}, uX::Tuple{ModalTrav{TV, Matrix{TV}}, Matrix{TV}}, ua::Function, errors=[]) where TV
 
     (Z, T, F, α, ρ) = ZTFαρ
     (u, X) = uX
 
-    err1 = collect_errors((T, F, α, ρ), X, ua, [], 0)
+    err1 = collect_errors((T, F, α, ρ), X, ua, [])
     err2 = collect_errors(Z, u, ua)
 
-    # append!(errors, max(err1[1],err2[1]))
-    return (err1[1],err2[1])
+    append!(errors, max(err1[1],err2[1]))
+    # return (err1[1],err2[1])
 end
